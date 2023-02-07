9cd9ceb3dc64d6618ab296f62d92ffadd90aaff5
Elaminebendaas <33202205+Elaminebendaas@users.noreply.github.com>
2023-02-07 13:04:25 -0500

    created nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Footer from './components/Footer'
+import Navbar from './components/NavBar'
 
 function App() {
 
 
   return (
     <div className="App">
+      <Navbar/>
       <Footer/>
     </div>
   )
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import '../App.css'
+
+function Navbar(){
+    return(
+        <div className='navbar'>
+            <h1 className='title'>To Do</h1>
+            <button>Add Task</button>
+        </div>
+    )
+}
+export default Navbar
